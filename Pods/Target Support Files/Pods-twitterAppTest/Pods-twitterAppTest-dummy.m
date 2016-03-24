a24e0be0cbdc9ef7a34825448fd54da25dc657f1
jackynguyen110 <jackynguyen110@gmail.com>
2016-03-24 17:40:53 +0700

    update authorize

---
+++ Pods/Target Support Files/Pods-twitterAppTest/Pods-twitterAppTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_twitterAppTest : NSObject
+@end
+@implementation PodsDummy_Pods_twitterAppTest
+@end
--- twitterAppTest/ViewController.swift
+++ twitterAppTest/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import BDBOAuth1Manager
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+           }
 
+    @IBAction func onLoginTapped(sender: AnyObject) {
+        let twiterClient = BDBOAuth1SessionManager(baseURL: NSURL(string: "https://api.twitter.com")!, consumerKey: "1q9ZuW7Kn9j1GSUctCOO2LZuh", consumerSecret: "TH7jVjkyQcy4Fv62ZOZTPaDVqkkgZ10JsoxYUEFgm8iDM9M1Gu")
+        
+        twiterClient.deauthorize()
+        twiterClient.fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: nil, scope: nil, success: { (requestToken: BDBOAuth1Credential!) -> Void in
+            print("i got a token")
+            let url = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")!
+            UIApplication.sharedApplication().openURL(url)
+            }) { (error:NSError!) -> Void in
+                print(error.localizedDescription)
+        }
+
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
