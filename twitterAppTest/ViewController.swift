ec3d4b2512ab0ec42840289990fc4801b4c4aaf3
jackynguyen110 <jackynguyen110@gmail.com>
2016-03-24 11:15:08 +0700

    Base Source 11-14 24-3

---
+++ twitterAppTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitterAppTest
+//
+//  Created by DEREK DO on 24/3/16.
+//  Copyright © 2016 jackyCode.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
