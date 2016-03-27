ea1d4578285f4f4bd03c2c2a39ea89108e68d9f4
jackynguyen110 <jacky.nguyen110@gmail.com>
2016-03-27 23:51:39 +0700

    new source

--- Podfile
+++ Podfile
@@ -3,7 +3,8 @@
 # Uncomment this line if you're using Swift
 use_frameworks!
 
-target 'twitterAppTest' do
-pod 'AFNetworking’
-pod ‘BDBOAuth1Manager’
+target 'TwitterClient-Assignment#3' do
+  pod "AFNetworking"
+  pod "BDBOAuth1Manager"
 end
+
---
+++ TwitterClient-Assignment#3/UpdateViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UpdateViewController.swift
+//  TwitterClient-Assignment#3
+//
+//  Created by jacky nguyen on 3/27/16.
+//  Copyright © 2016 jacky nguyen. All rights reserved.
+//
+
+import UIKit
+
+class UpdateViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
